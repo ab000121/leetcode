1c18b8e992f7bac364dabc8ad64945eb70f953b6
Anuj Bisht <bishtanuj423@gmail.com>
2026-07-07 13:46:18 +0530

    Time: 3 ms (66.42%), Space: 46.6 MB (33.12%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -17,7 +17,7 @@ class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if(root == null) return false;
         if(root.val == subRoot.val && check(root , subRoot)){
-            return check(root , subRoot);
+            return true;
         }
         return isSubtree(root.left , subRoot) || isSubtree(root.right , subRoot);
     }
